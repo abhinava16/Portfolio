9a4f8d709a621f083edcb979636fd1523130bae8
abhinava16 <abhinavareddy16@gmail.com>
2024-05-16 16:41:57 -0500

    commit changes

--- src/Github/Github.jsx
+++ src/Github/Github.jsx
@@ -3,8 +3,6 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
 
-  const data = useLoaderData()
-
   // const [data, useData] = useState([])
 
   // useEffect(() => {
@@ -15,8 +13,8 @@ function Github() {
   //     useData(data)
   //   })
   // }, [])
-  
 
+  const data = useLoaderData()
   return (
     <div className='text-center text-white bg-gray-700 text-3xl px-4 py-3'>My Github Name: {data.name}
     <img src={data.avatar_url} alt='my_github_profile_pic' width='300' height='300' className=' p-4'/>
